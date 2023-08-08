788509ca95ce17d7c8c660190ab78f03359db167
Mihir Rane <ranemihirk@gmail.com>
2023-08-09 01:03:29 +0530

    made changes to tailwind css config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.tsx"],
+  content: ["./index.html",
+  "./src/**/*.{js,ts,jsx,tsx}",],
   plugins: [require('@tailwindcss/forms'), require("daisyui")],
   daisyui: {
     darkTheme: 'dark',
