37c131f40fc756c255bfbeedd9f2b2476720d902
Mihir Rane <ranemihirk@gmail.com>
2023-08-22 03:02:39 +0530

    Updated service worker

--- public/sw.js
+++ public/sw.js
@@ -1,21 +1,24 @@
 const CACHE_NAME = "raorane-cache";
+const urlsToCache = [
+    '/static/js/bundle.js',
+    '/static/js/vendors-node_modules_react-router-dom_dist_index_js.chunk.js',
+    '/static/js/vendors-node_modules_fortawesome_free-solid-svg-icons_faBars_js-node_modules_fortawesome_free-aad28d.chunk.js',
+    '/static/js/CoreLayout.chunk.js',
+    '/static/js/Router.chunk.js',
+    '/index.html',
+    '/',
+];
 
-this.addEventListener('install', (event) => {
+this.addEventListener('install', event => {
     event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            cache.addAll([
-                '/static/js/bundle.js',
-                '/static/js/vendors-node_modules_react-router-dom_dist_index_js.chunk.js',
-                '/static/js/vendors-node_modules_fortawesome_free-solid-svg-icons_faBars_js-node_modules_fortawesome_free-aad28d.chunk.js',
-                '/static/js/CoreLayout.chunk.js',
-                '/static/js/Router.chunk.js',
-                'https://cdn.tailwindcss.com/3.3.3',
-                '/index.html',
-                '/'
-            ])
-        })
-    )
-})
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('caches: ', caches);
+                console.log('cache: ', cache);
+                cache.addAll(urlsToCache)
+            })
+    );
+});
 
 this.addEventListener('fetch', (event) => {
     if (!navigator.onLine) {
