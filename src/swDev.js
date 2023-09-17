356749306461037bae1ee1db9d9a93a4db51a0ec
Mihir Rane <ranemihirk@gmail.com>
2023-09-18 02:34:32 +0530

    Deleted the service worker files.

--- src/swDev.js
+++
@@ -1,8 +0,0 @@
-export default function swDev() {
-    console.log('process: ', process.env);
-    let swURL = `${process.env.PUBLIC_URL}/sw.js`;
-    console.log('swURL: ', swURL);
-    navigator.serviceWorker.register(swURL).then((response) => {
-        console.log('response: ', response);
-    });
-}
